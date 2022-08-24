de976021673d258f21536b17f1c2e3d1f3c030f2
alexander1fein <feinberg.alexander@gmail.com>
2022-08-24 18:27:34 -0400

    Done

--- promises.js
+++ promises.js
@@ -45,31 +45,66 @@ waitPromise()
 /* -------------------------- exploring then --------------------------- */
 
 // Your code here
-
+new Promise((resolve)=>{
+    setTimeout(()=>resolve('My Promise'),1000)
+}).then(result=>console.log(result))
 
 
 /* ============================== Phase 5 ============================== */
 /* ------------------- turn setTimeout into a Promise ------------------ */
 
 // Your code here
+function wait(ms){
+    return new Promise((resolve)=>setTimeout(resolve, ms))
+}
 
+async function func2(){
+    await wait(2000);
+    console.log("Waited the 2 seconds.")
 
-
+}
+func2()
 /* ============================== Phase 6 ============================== */
 /* -------------------- exploring reject and .catch -------------------- */
 
 // Your code here
+const returnPromise = async(val)=>{
+    return new Promise((resolve, reject)=>{
+        if(val>0.5) resolve("Success")
+        else reject("Failed")
 
+    })
+
+}
+for(let i=0; i<10; i++){
+    const random = Math.random()
+    wait(2000).then(()=> returnPromise(random)).then(result=>console.log(result)).catch(error=>console.error("Error ",error))
+
+}
 
 
 /* ============================== Phase 7 ============================== */
 /* ---------------- exploring async/await and try/catch ---------------- */
 
 // Your code here
+const returnPromiseAsync = async(i)=>{
+    const random = Math.random()
+    await wait(2000)
+    try{
+        const result = await returnPromise(random)
+        console.log("Async Success, ", i, result)
+    }catch(e){
+        console.log("Async Fail ", i, e)
+    }
+}
 
+for(let i=0; i<10; i++){
+    returnPromiseAsync(i)
+}
 
 
 /* ============================== Phase 8 ============================== */
 /* -------------------- Promises are asynchronous! --------------------- */
 
 // Your code here
+console.log("END OF PROGRAM")
