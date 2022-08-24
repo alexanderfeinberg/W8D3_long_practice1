09b53af9824bdd2aa8070c5ec002cac2421b8f60
alexander1fein <feinberg.alexander@gmail.com>
2022-08-24 17:28:53 -0400

    Halfway

--- promises.js
+++ promises.js
@@ -3,20 +3,42 @@
 
 // Your code here
 
+function func1(){
+    return 1;
+}
 
+async function async1(){
+    return 2;
+}
 
+console.log(func1())
+console.log(async1())
+async1().then(result=>console.log(result))
 /* ============================== Phase 2 ============================== */
 /* -------------------------- exploring await -------------------------- */
 
 // Your code here
 
-
-
+async function waiting(){
+    const val = await async1();
+    console.log(val)
+}
+waiting()
 /* ============================== Phase 3 ============================== */
 /* --------------------- creating a custom Promise --------------------- */
 
 // Your code here
 
+async function waitPromise(){
+
+    const myPromise = new Promise((resolve, reject)=>{
+        setTimeout(()=>resolve('New Promise'), 1000)
+    })
+    const result = await myPromise
+    console.log('my promise is ', result);
+}
+
+waitPromise()
 
 
 /* ============================== Phase 4 ============================== */
